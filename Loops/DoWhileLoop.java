7c745f1e673b60c288a8b34cdd932846059295e6
Mrvishal03 <mrvishal152207@gmail.com>
2024-07-16 00:41:28 +0530

    Loops In Java

---
+++ Loops/DoWhileLoop.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+       
+       int i =12;
+        do {
+            System.out.println(i);
+            i++;
+            
+        } while (i<=10);
+    }
+    
+
+}
