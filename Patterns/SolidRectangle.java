566d66cdd00dc99cde42716f4c377dc3fd3123ae
Mrvishal03 <mrvishal152207@gmail.com>
2024-07-16 23:08:38 +0530

    Patterns In Java

---
+++ Patterns/SolidRectangle.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+import Loops.forLoop;
+
+public class SolidRectangle {
+    public static void main(String[] args) {
+        for(int i = 0;i<4;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+    
+}
+
